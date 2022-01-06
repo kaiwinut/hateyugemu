16597e2b3e05773d749c944a1ad5a0523857131b
Kai Wang <kevin.wang.part.time@gmail.com>
2022-01-07 06:32:40 +0900

    Update for v0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding = 'utf-8') as fh:
 
 setuptools.setup(
 	name = "hateyugemu",
-	version = "0.0.2",
+	version = "0.0.3",
 	author = "Kai Wang",
 	license = "MIT",
 	description = "A board game.",
